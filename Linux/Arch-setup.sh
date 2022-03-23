5f65f796afb702db7c4e4a8dfa14b00c6a055e54
NoirJ0e <noirjoe@outlook.com>
2022-03-23 19:32:03 +0800

    Update

--- Linux/Arch-setup.sh
+++ Linux/Arch-setup.sh
@@ -1,14 +1,18 @@
 # Setup the archlinuxcn mirror
 echo "Server = https://mirror.sjtu.edu.cn/archlinux/\$repo/os/\$arch" >> /etc/pacman.d/mirrorlist
+# prepare
 pacman -Sy --noconfirm
 pacman -S archlinuxcn-keyring --noconfirm
-pacman -S paru --noconfirm
+pacman -S paru rsync --noconfirm
+
+# === prepare the config for each program
 # For nvim
 mkdir -p $HOME/.config/nvim
 # For ranger
 mkdir -p $HOME/.config/ranger
-# Make sure has rsync
-paru -S rsync ueberzug flameshot fcitx5 fcitx5-rime fcitx5-qt fcitx5-gtk yakuake trash-cli brave-bin obsidian xournalpp texlive-most
-
+# 
 rsync -a nvim/ $HOME/.config/nvim
 rsync -a ranger/ $HOME/.config/ranger
+# Install applications
+paru -S ueberzug flameshot fcitx5 fcitx5-rime fcitx5-qt fcitx5-gtk yakuake trash-cli brave-bin obsidian xournalpp texlive-most
+
