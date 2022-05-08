6256e830250a2d17fa08871707cd660f5fbaaa6a
NoirJ0e <noirjoe@outlook.com>
2022-05-08 15:30:41 +0800

    Update on Macbook Pro

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,7 @@
+local wezterm = require 'wezterm';
+return {
+        font = wezterm.font("JetBrainsMono Nerd Font Mono", {weight="Regular", italic=False,}),
+        font_size = 16.0,
+        color_scheme = "Dracula",
+        cursor_blink_rate = 800,
+}
