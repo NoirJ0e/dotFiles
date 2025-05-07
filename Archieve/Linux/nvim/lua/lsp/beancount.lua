03de17a36a2d0121f89475a7e2f9d606e0fe81f8
NoirJ0e <noirjoe@outlook.com>
2025-05-07 14:35:27 +1000

    remove all legacy files

--- Archieve/Linux/nvim/lua/lsp/beancount.lua
+++
@@ -1,7 +0,0 @@
-return{}
--- local lspconfig = require 'lspconfig'
--- lspconfig.beancount.setup= {
---     init_options = {
---         journal_file = "/home/noir/.beancount-lsp",
---     };
--- };
--- Archieve/wezterm/wezterm.lua
+++
@@ -1,7 +0,0 @@
-local wezterm = require 'wezterm';
-return {
-        font = wezterm.font("JetBrainsMono Nerd Font Mono", {weight="Regular", italic=False,}),
-        font_size = 16.0,
-        color_scheme = "Dracula",
-        cursor_blink_rate = 800,
-}
