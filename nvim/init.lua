caedaba2c1bc40adef8c3dd8473f13e37851bd1c
NoirJ0e <noirjoe@outlook.com>
2025-10-03 14:35:28 +1000

    update

--- nvim/init.lua
+++ nvim/init.lua
@@ -25,3 +25,26 @@ vim.g.neovide_normal_opacity = 0.5
 vim.g.neovide_background_color = ("#0f1117" .. string.format("%x", math.floor(((255 * vim.g.neovide_opacity) or 0.8))))
 vim.g.neovide_window_blurred = true
 vim.o.guifont = "JetBrainsMono Nerd Font:h14"
+
+-- Disable wrap and textwidth for markdown files
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "markdown",
+  callback = function()
+    vim.opt_local.wrap = false
+    vim.opt_local.textwidth = 0
+  end,
+})
+
+-- Optionally, set wrap and textwidth for all other filetypes (if desired)
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "*",
+  callback = function()
+    if vim.bo.filetype ~= "markdown" then
+      vim.opt_local.wrap = true
+      vim.opt_local.textwidth = 80
+    end
+  end,
+})
+
+-- vim.g.clipboard = "osc52"
+-- vim.cmd.colorscheme "catppuccin-macchiato"
--- nvim/lua/community.lua
+++ nvim/lua/community.lua
@@ -10,6 +10,30 @@ return {
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.pack.markdown" },
   { import = "astrocommunity.pack.java" },
+  -- {
+  --   "mfussenegger/nvim-jdtls",
+  --   opts = {
+  --     settings = {
+  --       java = {
+  --         configuration = {
+  --           runtimes = {
+  --             {
+  --               name = "zulu-21",
+  --               path = "/usr/lib/jvm/java-11-openjdk/",
+  --             },
+  --           },
+  --         },
+  --       },
+  --       format = {
+  --         enabled = true,
+  --         settings = { -- you can use your preferred format style
+  --           url = "https://raw.githubusercontent.com/google/styleguide/gh-pages/eclipse-java-google-style.xml",
+  --           profile = "GoogleStyle",
+  --         },
+  --       },
+  --     },
+  --   },
+  -- },
   -- { import = "astrocommunity.pack.jj" },
   { import = "astrocommunity.pack.json" },
   { import = "astrocommunity.pack.go" },
